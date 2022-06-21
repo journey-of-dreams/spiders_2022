eeadb28ebac61c675c2cd17ee6f7da2e54015da3
jincanyu <927518044@qq.com>
2022-06-21 19:59:17 +0800

    修复

--- spider2022/items.py
+++ spider2022/items.py
@@ -8,6 +8,7 @@ import scrapy
 
 class MovieItem(scrapy.Item):
     # define the fields for your item here like:
+    # #Field()：携带存储元数据
     # name = scrapy.Field()
     title = scrapy.Field()
     rating = scrapy.Field()
--- spider2022/spiders/douban.py
+++ spider2022/spiders/douban.py
@@ -16,10 +16,14 @@ class DoubanSpider(scrapy.Spider):
     def parse(self, response: HtmlResponse):
         # 通过selector包裹成一个选择器对象
         sel = Selector(response)
+        # el.xpath
+        # 取得单一元素值呼叫get（）方法/取得多个元素值呼叫getall（）方法/取得文字内容，加上「/ text（）」关键字/取得属性值则加上「@属性名称」关键字
+        #  sel.css
+        # sel.re
         list_items = sel.css('#content > div > div.article > ol > li')
         for list_item in list_items:
             movie_item = MovieItem()
-            # ::text：获取文本信息，extract_first()：获取数组的第一条数据
+            # ::text：获取文本信息，extract：将解析器列表转化为为字符串数组，extract_first()：获取解析器列表的第一条数据
             movie_item['title'] = list_item.css('span.title::text').extract_first()
             movie_item['rating'] = list_item.css('span.rating_num::text').extract_first()
             movie_item['subject'] = list_item.css('span.inq::text').extract_first()
